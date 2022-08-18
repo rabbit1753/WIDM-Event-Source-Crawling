0a511ee54e14c0e4cdeda4f6f44d820bc7305a24
yuming <yu4911972@gmail.com>
2022-08-18 21:29:24 +0800

    處理完reward以及loss的計算

--- Global_Network.py
+++ Global_Network.py
@@ -1,161 +1,97 @@
+"""
+https://jovian.ai/tobiasschmidbauer1312/asynchronous-actor-to-critic-ac3
+"""
 import numpy as np
-import random
-import torch as t
+import torch
 import torch.nn as nn
 from torch.nn import functional as F
-import matplotlib.pyplot as plt
+from torch.distributions import Categorical
 
+class ShareAdam(torch.optim.Adam):
+    def __init__(self, params, lr=0.001,  betas=(0.9, 0.99), eps=1e-8, weight_decay=0):
+        super(SharedAdam, self).__init__(params, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay) #explain what is going on here
 
-criterion = nn.CrossEntropyLoss()
-gamma = 0.9
-actor_lr = 0.001
-critic_lr = 0.01
-epochs = 500
+        for group in self.params_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'] = 0
+                state['exp_avg'] = torch.zeros_like(p.data)
+                state['exp_avg_sq'] = torch.zeros_like(p.data)
 
-class share_layer(nn.Module):
-    def __init__(self):
+                state['exp_avg'].share_memory_()
+                state['exp_abg_sq'].share_memory_()
+
+class ActorCritic(nn.Module):
+    def __init__(self, input_dims, n_links, gamma):
+        super(ActorCritic, self).__init__()
+
+        self.gamma = gamma
+
+        self.actor_layer1 = nn.Linear(input_dims, 128)
+        self.actor_layer = nn.Linear(128, n_links)
         
-        super(share_layer, self).__init__()
-        self.linear1 = nn.Linear(4, 20)
-        nn.init.normal_(self.linear1.weight, 0, 0.1)
-        nn.init.constant_(self.linear1.bias, 0.1)
+        self.critic_layer1 = nn.Linear(input_dims, 128)
+        self.crtitc_layer = nn.Linear(128, n_links)   
 
-    def forward(self, out):
-        
-        out = self.linear1(out)
-        out = F.relu(out)
-
-        return out
+        self.rewards = []
+        self.links = []
+        self.state = [[]for i in range(10000)]
     
-class Actor(nn.Module):
-    def __init__(self, sl):
+    def forward(self, state):  #stste要從frontier所有links當states還是只把當前page的links當states
+        actor_layer1 = F.relu(self.actor_layer1(state))
+        probability = torch.sigmoid(self.actor_layer(actor_layer1)) #dont discuss with other link,so use sigmoid
         
-        super(Actor, self).__init__()
-
-        self.share_layer = sl
-
-        self.linear2 = nn.Linear(20, 2)
-        nn.init.normal_(self.linear2.weight, 0 ,0.1)
-        nn.init.constant_(self.linear2.bias, 0.1)
-
-    def forward(self, x):
+        critic_layer1 = F.relu(self.critic_layer1(state)) 
+        scores = self.actor_layer(critic_layer1)
         
-        out = t.from_numpy(x).float()
-
-        out = self.share_layer(out)
-        out = self.linear2(out)
-        prob = F.softmax(out, dim = 1)
-        return prob, out
-
-class Critic(nn.Module):
-
-    def __init__(self, sl):
-
-        super(Critic, self).__init__()
-
-        self.share_layer = sl
-
-        self.linear2 = nn.Linear(20, 1)
-        nn.init.normal_(self.linear2.weight, 0, 0.1)
-        nn.init.constant_(self.linear2.bias, 0.1)
+        return probability, scores
     
-    def forward(self, x):
+    def record_episode(self, reward, link, state_count):
+        self.rewards.append(reward)
+        self.links.append(link)
+        self.state[state_count].append(reward,link) 
 
-        out = t.from_numpy(x).float()
+    def clear_memory(self):
+        self.rewards = []
+        self.links = []
+        self.state = [[]for i in range(10000)]
 
-        out = self.share_layer(out)
+    def calc_R(self, done):   #done from event estimator conclus
+        state = torch.tensor(self.states, dtype = torch.float)
+        _,  value= self.forward(state)
 
-        out = self.linear2(out)
+        award = value[-1]*(1-int(done))  #dont know how to handle ,maybe game characteristic
 
-        return out
-
-def choose_action(prob):
+        reward_record = []
+        for reward in self.rewards[::-1]:
+            award = reward + self.gamma*award
+            reward_record.append(award)
+        
+        return reward_record
     
-    action = np.random.choice(a = 2, p = prob[0].detach().numpy())
-    return action
+    def calc_loss(self, done): #cal loss function
+        state = torch.tensor(self.states, dtype=torch.float)
+        #actions = torch.tensor(self.actions, dtype=torch.float)
 
-def Actor_learn(optim, critic, s, s_, a, r, logits):
+        reward = self.calc_R(done)
 
-    # s : 當前狀態
-    # s_ : 下一個狀態
-    # a : 當前執行的動作
-    # r : reward
+        _ , critic = self.forward(state)
+        values = critic.squeeze()
+        critic_loss = (reward - values) 
+        """
+        probs = torch.sigmod(actor, dim = 1)
+        dist = Categorical(probs)
+        log_probs = dist.log_prob(actions)
+        actor_loss = -log_probs*(reward - values)
+        """
+        total_loss = critic_loss
+        return total_loss
 
-    V_s = critic(s)
-    V_s_ = critic(s_)
-
-    a = t.tensor([a]).long()
-    logp_a = criterion(logits, a)
-
-    l = r + V_s - V_s_
-    l = l.item()
-    loss = l * logp_a
-
-    optim.zero_grad()
-
-    loss.backward()
-    optim.step()
-
-def Critic_learn(optim, critic, s, r, s_):
-
-    V_s = critic(s)
-    V_s = critic(s_)
-
-    loss = (r + gamma * V_s_.item() - V_s)**2
-    optim.zero_grad() # 清空上一輪的gradient，
-    loss.backward() # 
-
-    optim.step()
-
-    return r + gamma * V_s_.item() - V_s
-
-def learn():
-
-    sl = share_layer()
-    actor = Actor(sl)
-    critic = Critic(sl)
-
-    actor.train()
-    critic.train()
-
-    actor_optim = t.optim.Adam(actor.parameters(), lr = actor_lr)
-    critic_optim = t.optim.Adam(critic.parameters(), lr = critic_lr)
-    train_reward = []
-
-    for i in range(epochs):
-
-        # state = env.reset()
-        done = False
-        sum_rewards_i = 0
-        step = 0
-
-        while not done:
-
-            step += 1
-
-            # env.render()
-            state = np.expand_dims(state, axis = 0)
-
-            prob, logits = actor(state)
-
-            action = choose_action(prob)
-
-            # state_, reward, done, info = env.step(action)
-            # if done:
-            #     reward = -20.0
-            sum_rewards_i += reward
-
-            l = Critic_learn(critic_optim, critic, state, reward, state_)
-            Actor_learn(actor_optim, criticm, state, state_, action, reward, logits)
-            state = state_
-
-        train_reward.append(sum_rewards_i)
-        print("epoch is :", i)
-        print("step nums is :", step)
+    # def choose_action(prob): #frontier solve?
+        
+    #     action = np.random.choice(a = 2, p = prob[0].detach().numpy())
+    #     return action
 
 if __name__=="__main__":
 
-    learn()
-    # env.close()
-
-
+    print("HI")
