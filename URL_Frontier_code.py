4d33db72afe058d50707a415f69d19c50f8566d3
cyun <cyun40717@gmail.com>
2022-08-18 21:09:08 +0800

    add feature

--- URL_Frontier_code.py
+++ URL_Frontier_code.py
@@ -13,16 +13,20 @@ class URL_Frontier:
         self.url_probability = {}
         self.estimator_score = {}
         self.fin_score = {}
-        
+        self.feature = {} 
+        self.returned = ''
     def process_list( self , URL_list ) :
+        #被呼叫函數，處理傳過來的list
         URL_list = eval(URL_list)
         
         for val in URL_list :
             self.url_probability[ val[0] ] = val[1]
             
-            self.estimator_score[ val[0] ] = val[2]  
-        
-    def return_link( self ) :
+            self.estimator_score[ val[0] ] = val[2] 
+            
+            self.feature[ val[0] ] = val[3]  
+
+    def discriminate( self ) :
         
         score_list = preprocessing.scale(list(self.url_probability.values()))
         """
@@ -33,14 +37,24 @@ class URL_Frontier:
         for key,score_value in zip(self.url_probability.keys() , score_list) :
             self.fin_score[ key ] = float(self.url_probability[ key ]) * float(score_value)
         
-        returned = max(self.fin_score, key=self.fin_score.get)
-
-        self.url_probability.pop( returned )
-        self.estimator_score.pop( returned )
-        self.fin_score.pop( returned )     
-        # 刪除回傳的link
-        return returned
-
+        self.returned = max(self.fin_score, key=self.fin_score.get)
+        if self.fin_score[ self.returned ] >= 0.7 :
+            #大於門檻值0.7會回傳True
+           return True
+        return False
+    
+    def return_link( self ) :        
+        
+        self.url_probability.pop( self.returned )
+        self.estimator_score.pop( self.returned )
+        self.fin_score.pop( self.returned )  
+        self.feature.pop( self.returned )  
+        
+        return self.returned
+    
+    def return_feature( self ):
+        
+        return list(self.feature.values())
 
 
 
