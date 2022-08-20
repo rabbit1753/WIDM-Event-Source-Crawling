c1ef7e8f4f781800d9d3a335c5d1927aa1750e4e
yuming <yu4911972@gmail.com>
2022-08-20 17:26:02 +0800

    成功讀取所有seed，返回seed中最新的一個link

--- job_assignment.py
+++ job_assignment.py
@@ -1,13 +1,18 @@
-seed = ["http://careernthu.conf.asia/",
-            "http://fishbar.com.tw",
-            "https://imc.ichiayi.com"]
+from asyncio.windows_events import NULL
+import csv
+import pandas as pd
 
-def job_assignment():
-    link = seed[0]
-    del seed[0]
-    return link
+def job_assignment(count):
+    seed = pd.read_csv('train.csv')
+    link = seed['Event Message Page URL']
+    if count >= len(link):
+        print("no seed to crawling")
+        return 0
+    return link[count]
 
 if __name__ == '__main__': 
-    for i in range(len(seed)):
-        link = job_assignment()
+    for i in range(400):
+        link = job_assignment(i)
+        if link == 0:
+            break
         print(link)
