9da21b24ab64782ed91cb0e4f2cd55b9974e8830
rabbit1753 <yinming852@gmail.com>
2022-09-01 14:39:59 +0800

    整合 and 修復 bug

--- Global_Network.py
+++ Global_Network.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from torch.distributions import Categorical
 
 class ShareAdam(torch.optim.Adam):
     def __init__(self, params, lr=0.001,  betas=(0.9, 0.99), eps=1e-8, weight_decay=0):
@@ -32,31 +33,26 @@ class ActorCritic(nn.Module):
         self.crtitc_layer = nn.Linear(128, 1)   
 
         self.rewards = []
-        self.links = []
+        self.actions = []
         self.state = []
         
     def forward(self, state):
-        print("執行過forward")
-        # print(state.shape)
         actor_layer1 = F.relu(self.actor_layer1(state))
-        # probability = self.actor_layer(actor_layer1)
         probability = torch.sigmoid(self.actor_layer(actor_layer1)) #dont discuss with other link,so use sigmoid
-        print("執行過這裡")
-        # critic_layer1 = F.relu(self.critic_layer1(state[count]))
+
         critic_layer1 = F.relu(self.critic_layer1(state))
-        # score = self.critic_layer(critic_layer1)
         scores = torch.sigmoid(self.crtitc_layer(critic_layer1))
         
         return probability, scores
     
-    def record_episode(self, reward, link, feature):
+    def record_episode(self, reward, action, feature):
         self.rewards.append(reward)
-        self.links.append(link)
+        self.actions.append(action)
         self.state.append(feature)
 
     def clear_memory(self):
         self.rewards = []
-        self.links = []
+        self.actions = []
         self.state = []
 
     def calc_R(self, state):   #done from event estimator #要改
@@ -73,32 +69,46 @@ class ActorCritic(nn.Module):
         
         return reward_record
     
-    def calc_loss(self): #cal loss function  #要改
-        print("執行過loss")
+    def calc_loss(self):
+
+        actions = torch.tensor(self.actions)
 
         temp = []
+        ma = 0
         for i in range(len(self.state[0][0])):
             temp.append(0)
         temp = torch.tensor(temp)
         temp = torch.unsqueeze(temp,0)
-        for i in range(len(self.state)-1):
-            for j in range(len(self.state[i]),len(self.state[-1])):
-                self.state[0] = torch.cat((self.state[i],temp),0)
-        
+        for i in range(len(self.state)):
+            max_temp = len(self.state[i])
+            if ma < max_temp:
+                ma = max_temp
+                max_index = i
+        for i in range(len(self.state)):
+            if i == max_index:
+                continue
+            for j in range(len(self.state[i]),len(self.state[max_index])):
+                self.state[i] = torch.cat((self.state[i],temp),0)
         state = torch.tensor([item.detach().numpy() for item in self.state])
+        
+        # actions = torch.tensor(self.actions, dtype=torch.float)
 
-        reward = self.calc_R(state)
+        acc = self.calc_R(state)
+        acc = torch.tensor([item.detach().numpy() for item in acc])
+        actor , critic = self.forward(state)
+        predict = critic.squeeze() 
+        acc = torch.reshape(acc, (acc.shape[0],acc.shape[1]))
 
-        _ , critic = self.forward(state)
-        acc = critic.squeeze()
-        critic_loss = (reward - acc) 
-        """ 用不到原因：我們的action並不固定，並不是像遊戲裡有固定的上下左右可以選擇，每次選取完link之後，被選取的link都不會再被重複選取，因此不能使用下面的code
-        probs = torch.sigmod(actor, dim = 1)
+        critic_loss = (acc - predict)**2
+        # print(critic_loss)
+
+        probs = torch.sigmoid(actor)
         dist = Categorical(probs)
         log_probs = dist.log_prob(actions)
         actor_loss = -log_probs*(reward - values)
-        """
-        total_loss = critic_loss
+
+        total_loss = critic_loss + actor_loss
+        print(total_loss)
         return total_loss
 
 if __name__=="__main__":
